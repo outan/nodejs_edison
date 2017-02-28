70427b784eca7b672949a58f8ab1493d1627b7de
outan <outannexway@gmail.com>
2017-02-28 17:25:27 +0900

    change file name to beacon.js

--- retailtech/beacon.js
+++ retailtech/beacon.js
@@ -1,12 +1,30 @@
 var noble = require('noble');
 var mraa = require('mraa')
-
+//BLEのための変数宣言
+var serviceUUIDs = ['1230010039fa4005860c09362f6169da'];
+var characteristicUUIDs = ['1230010139fa4005860c09362f6169da'];
 var myOnboardLed = new mraa.Gpio(13); 
 myOnboardLed.dir(mraa.DIR_OUT); 
 
-//BLEのための変数宣言
-var serviceUUIDs = ['1230010039fa4005860c09362f6169da'];//Heart RateサービスのUUID
-var characteristicUUIDs = ['1230010139fa4005860c09362f6169da'];//Heart Rate Measurementに設定するキャラクタリスティクのUUID
+var five = require("johnny-five");
+var Edison = require("galileo-io");
+
+var board = new five.Board({io: new Edison()});
+board.on("ready", function() {
+  var http = require( 'http' );
+  var socketio = require( 'socket.io' );
+  var fs = require( 'fs' );
+
+  // 3000番ポートでHTTPサーバーを立てる
+  var server = http.createServer( function( req, res ) {
+    res.writeHead(200, { 'Content-Type' : 'text/html' }); // ヘッダ出力
+    res.end( fs.readFileSync('./index.html', 'utf-8') );  // index.htmlの内容を出力
+  }).listen(3000);
+
+  var io = socketio.listen( server );
+  io.sockets.on( 'connection', function(socket) {
+    console.log('I am connectted with socketio');
+  });
 
 
 //ペリフェラル検索のイベントに対するコールバック設定
@@ -33,10 +51,14 @@ noble.on('discover', discoverPeripheral);
 //ペリフェラル発見時のコールバックメソッド
 function discoverPeripheral(peripheral) {
     console.log('discover peripheral: ' + peripheral);
-    if (peripheral.advertisement.localName === 'outan_ble01') { 
+    localName = peripheral.advertisement.localName;
+    if (localName.match(/outan/)) { 
        // console.log(peripheral.advertisement.localName + " is detected");
-        console.log('Found device with local name: ' + peripheral.advertisement.localName);
+        console.log('Found device with local name: ' + localName);
         console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+
+       io.sockets.emit("ble_button", localName);
+       console.log("emit " + localName);        
 
         noble.stopScanning();
        //setTimeout(noble.startScanning(serviceUUIDs), 3000)
@@ -73,3 +95,4 @@ function discoverPeripheral(peripheral) {
        //noble.startScanning(serviceUUIDs);
    });
 }
+});
